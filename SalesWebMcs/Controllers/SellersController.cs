5c154a9085e0e71285e72557d274f7e0a0d16687
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2023-06-26 07:01:15 -0300

    SellersController

---
+++ SalesWebMcs/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMcs.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
